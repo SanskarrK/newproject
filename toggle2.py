529b0fc4aa9678901a77064ccd752862b7053630
SanskarrK <karkisanskar791@gmail.com>
2025-07-07 20:12:53 +0545

    Create toggle2.py

---
+++ toggle2.py
@@ -0,0 +1,20 @@
+Marks =[]
+
+f1 = input("Enter Marks here:")
+Marks.append(f1)
+
+f2 = input("Enter Marks here:")
+Marks.append(f2)
+
+f3 = input("Enter Marks here:")
+Marks.append(f3)
+
+f4 = input("Enter Marks here:")
+Marks.append(f4)
+
+f5 = input("Enter Marks here:")
+Marks.append(f5)
+
+Marks.sort()
+
+print(Marks)
