b6938c9578da7032aa1834b9d060a6ed5bdcc469
SanskarrK <karkisanskar791@gmail.com>
2025-07-09 20:24:59 +0545

    dis name and lang

---
+++ dis/set1.py/set6.py
@@ -0,0 +1,28 @@
+dis = {}
+
+name = input("Enter your name: ")
+lang = input("Enter your language: ")
+
+dis.update({name: lang})
+
+name = input("Enter your name: ")
+lang = input("Enter your language: ")
+
+dis.update({name: lang})
+
+name = input("Enter your name: ")
+lang = input("Enter your language: ")
+
+dis.update({name: lang})
+
+name = input("Enter your name: ")
+lang = input("Enter your language: ")
+
+dis.update({name: lang})
+
+name = input("Enter your name: ")
+lang = input("Enter your language: ")
+
+dis.update({name: lang})
+
+print(dis)
