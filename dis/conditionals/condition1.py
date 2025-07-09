ae3d27f4aabded766b4356b0845283d4b8c5b14f
SanskarrK <karkisanskar791@gmail.com>
2025-07-09 20:52:46 +0545

    if else ladder

---
+++ dis/conditionals/condition1.py
@@ -0,0 +1,10 @@
+a = int(input("Enter a age: "))
+if (a>=18):
+    print("You are eligible to vote")
+
+elif (a<0):
+    print("Invalid age entered")
+
+else:
+    print("You are not eligible to vote")
+# This code checks if a person is eligible to vote based on their age.
