ea35c53d1990071b5fbc00831f9eb8553ab3fa13
SanskarrK <karkisanskar791@gmail.com>
2025-07-11 20:09:50 +0545

    looping and some change

---
+++ dis/loop in p/loop4.py
@@ -0,0 +1,7 @@
+l =[1,2,4,8,6,98,66,45]
+
+for item in l:
+    print(item)
+else:
+    print("Loop completed without interruption.")    
+    ## This code iterates through a list and prints each element.
